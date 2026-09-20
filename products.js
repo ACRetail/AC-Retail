ffbd8c2f0b52d168fd63c3199b99bfd6290d58dd
ACRetail <avichaware@gmail.com>
2026-09-20 09:32:50 +0530

    Rename 'Dairy & Products' to 'Dairy'

--- products.js
+++ products.js
@@ -228,7 +228,7 @@ function createCategoryFilters() {
             ]
         },
         {
-            name: "Dairy & Products",
+            name: "Dairy",
             aliases: [
                 "Dairy"
             ]
